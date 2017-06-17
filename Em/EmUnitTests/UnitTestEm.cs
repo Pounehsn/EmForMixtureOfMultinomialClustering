30ba13d8212ab0b5ce332cfb7ebca0aaa0ad47f5
Pouneh Soleimani <pouneh.sn@icloud.com>
2017-06-17 13:16:10 -0700

    test project skeleton

---
+++ Em/EmUnitTests/UnitTestEm.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace EmUnitTests
+{
+    [TestFixture]
+    public class UnitTestEm
+    {
+        [Test]
+        public void DumeTest()
+        {
+            Assert.AreEqual(4, 2 * 2);
+        }
+    }
+}
